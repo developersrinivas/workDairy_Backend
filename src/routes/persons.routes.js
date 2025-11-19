986bed17e60a5c73e2274ae125047466c2c83b7c
AyushSeva <ayushseva04@gmail.com>
2025-11-19 17:07:46 +0530

    build new one

--- src/routes/persons.routes.js
+++ src/routes/persons.routes.js
@@ -1,11 +1,12 @@
 // src/routes/persons.routes.js
 import express from 'express';
-import { list, create, update, remove } from '../controllers/persons.controller.js';
+import { list, create, update, remove, ledger } from '../controllers/persons.controller.js';
 const router = express.Router();
 
 router.get('/', list);
 router.post('/', create);
 router.put('/:id', update);
 router.delete('/:id', remove);
+router.get('/:id/ledger', ledger);
 
 export default router;
